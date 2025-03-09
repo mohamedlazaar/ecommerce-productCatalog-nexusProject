d7402fc1cb132e010990dd602653de80c649a456
mohamed lazaar <lazzarmohamed10@gmail.com>
2025-03-09 21:01:19 +0000

    ///

--- components/CategoriesSection.tsx
+++ components/CategoriesSection.tsx
@@ -8,17 +8,22 @@ import Link from 'next/link';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+// Define proper interface for Category
+interface Category {
+    id: string | number;
+    name: string;
+    category_image_url: string;
+    // Add other properties your category might have
+}
+
 const CategoriesSection = () => {
     const dispatch = useDispatch<AppDispatch>();
     const products = useSelector((state: RootState) => state.categories);
   
-
     useEffect(() => {
         dispatch(fetchCategories());
-        console.log("fet", dispatch(fetchCategories()))
+        // Remove duplicate dispatch call
     }, [dispatch]);
-
-
 
     const settings = {
         dots: true,
@@ -50,14 +55,15 @@ const CategoriesSection = () => {
         <div className="pt-[100px] pb-[100px] flex flex-col container mx-auto gap-10 ">
             <h2 className="text-4xl font-extrabold pl-[20px] mt-2 mb-6">Categories</h2>
             <Slider {...settings} className="w-full">
-                {products.list.map((category: any) => (
+                {products.list.map((category: Category) => (
                     <div key={category.id} className="px-2">
-                        <div className="group flex flex-col w-full h-[200px] items-center  rounded-[10px] border-[1px] relative overflow-hidden">
+                        <div className="group flex flex-col w-full h-[200px] items-center rounded-[10px] border-[1px] relative overflow-hidden">
+                            {/* You can switch to Next.js Image component to fix the warning, but keeping img here for now */}
                             <img 
                                 src={category.category_image_url} 
                                 className="w-full h-full rounded-[10px] object-cover group-hover:scale-110 transition-all ease-in-out" 
                                 alt={category.name}
-                            />  
+                            />
                             <div className='absolute w-full h-full top-0 left-0 bg-black opacity-40 rounded-[10px] z-1'></div>
                             <p className="font-extrabold text-center text-[20px] text-white mt-2 absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] z-2">
                                 {category.name}

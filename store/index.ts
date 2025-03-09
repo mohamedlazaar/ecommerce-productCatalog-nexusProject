7220a67ba9fdcec6f6a56948d0b1b70563ed71ce
mohamed lazaar <lazzarmohamed10@gmail.com>
2025-03-09 19:17:56 +0000

    ...

--- store/index.ts
+++ store/index.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from './productSlice';
-import authReducer from './authSlice'; 
 import categoryReducer from './categorySilce'
 import cartReducer from './cartSilce'
 export const store = configureStore({
     reducer: {
         products: productReducer,
-        auth: authReducer, 
         categories: categoryReducer,
         cart: cartReducer,
     },
